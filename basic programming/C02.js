1fe185ad7fe5d968a19aff04c1114ab3f737faa7
Student <Student@Train10-3-PC225.control.ad.sit.kmutt.ac.th>
2025-07-09 11:26:36 +0700

    class02 ตอนเช้า

---
+++ basic programming/C02.js
@@ -0,0 +1,20 @@
+// Recap C01
+var v;
+console.log(v == undefined);
+console.log(v === undefined);
+// today learning about Operator
+console.log(5 != 3)
+console.log(5 !== '5')
+console.log(5 != 5)
+console.log(5 !== 5)
+console.log("abc" + 5 - 3)
+console.log("50" + 5 - 10)
+console.log("50" + 5 * 10)
+console.log(5 + "23" - 3)
+console.log(50 + 5)
+console.log(50 + "5")
+console.log([1,2,3,4] + 1)
+console.log()
+console.log()
+console.log()
+console.log()
